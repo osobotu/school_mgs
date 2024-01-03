a6970189798909f2c0af326e51d7cae566a41e6f
Stephen Obotu Oduh <osobotu@gmail.com>
2024-01-03 20:29:13 +0100

    added readme

--- db/sqlc/teachers.sql.go
+++ db/sqlc/teachers.sql.go
@@ -126,7 +126,7 @@ func (q *Queries) ListTeachers(ctx context.Context, arg ListTeachersParams) ([]T
 }
 
 const updateTeacher = `-- name: UpdateTeacher :one
-UPDATE teachers 
+UPDATE teachers
 SET first_name = $2, last_name = $3, middle_name = $4, subject_id = $5, classes = $6
 WHERE id = $1
 RETURNING id, first_name, last_name, middle_name, subject_id, classes, created_at
